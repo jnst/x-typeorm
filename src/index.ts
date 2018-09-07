151f803195d632a8be59d6bb2450a9b76c51b8f1
jnst <jnst@outlook.com>
2018-09-07 19:29:44 +0900

    Use repository

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,28 @@ import 'reflect-metadata';
 import {createConnection} from 'typeorm';
 import {User} from './entity/User';
 
-createConnection()
-  .then(async connection => {
+(async function() {
+  let connection;
+
+  try {
+    connection = await createConnection();
+    const userRepository = connection.getRepository(User);
+
     console.log('Inserting a new user into the database...');
     const user = new User();
     user.firstName = 'Timber';
     user.lastName = 'Saw';
     user.age = 25;
-    await connection.manager.save(user);
+    await userRepository.save(user);
     console.log('Saved a new user with id: ' + user.id);
 
     console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
+    const users = await userRepository.find();
     console.log('Loaded users: ', users);
-
-    console.log('Here you can setup and run express/koa/any other framework.');
-  })
-  .catch(error => console.log(error));
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await connection.close();
+    console.log('closed.');
+  }
+})();
